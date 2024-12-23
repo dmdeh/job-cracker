e1f3e4fc6b5267338590a406c54ffb0b11211f74
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-12-23 16:53:43 +0900

    fix: useSearchParams로 수정

--- src/app/selection/topics/page.tsx
+++ src/app/selection/topics/page.tsx
@@ -1,15 +1,14 @@
 "use client";
 
-import { useParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
 
 export default function Topics() {
-  const params = useParams();
-
-  console.log(params); // 동적 경로에서 추출된 매개변수 확인
+  const searchParams = useSearchParams();
+  const topic = searchParams.get("topic");
 
   return (
     <div>
-      <h1>Topics Page</h1>
+      <h1>{topic} Page</h1>
     </div>
   );
 }
