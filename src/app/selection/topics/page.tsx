90f8bc5fc6c8be50a2f4e9497e93bbd002a1c9c8
schnee98 <jooeun06161@gmail.com>
2024-12-26 16:07:47 +0900

    refactor: 코드 정리

--- src/app/selection/topics/page.tsx
+++ src/app/selection/topics/page.tsx
@@ -12,17 +12,6 @@ export default function Topics() {
   const router = useRouter();
   const [selected, setSelected] = useState<string[]>([]);
 
-  function isTopic(value: string | null): value is TopicKey {
-    return value !== null && Object.keys(TOPIC_MAP).includes(value);
-  }
-
-  function getTopic(value: string | null): TopicKey | null {
-    if (isTopic(value)) {
-      return value;
-    }
-    return null;
-  }
-
   const topic = getTopic(searchParams.get("topic"));
   const selectedTopics = topic ? TOPIC_MAP[topic] : {};
   const allTopics = Object.keys(selectedTopics);
@@ -54,18 +43,13 @@ export default function Topics() {
         <h1>어떤 주제로 {topic} 면접을 원하시나요?</h1>
       </div>
       <div className={styles.grid}>
-        <div
-          className={clsx(styles.card, { [styles.selected]: isAllSelected })}
-          onClick={toggleSelectAll}
-        >
+        <div className={cardClass(isAllSelected)} onClick={toggleSelectAll}>
           <h3>전체 선택</h3>
         </div>
         {allTopics.map((topic) => (
           <div
             key={topic}
-            className={`${styles.card} ${
-              selected.includes(topic) && styles.selected
-            }`}
+            className={cardClass(selected.includes(topic))}
             onClick={() => toggleSelectTopic(topic)}
           >
             <h3>{topic}</h3>
@@ -76,9 +60,7 @@ export default function Topics() {
         backgroundColor="var(--color-background-light)"
         width={100}
         height={50}
-        className={clsx(styles.button, {
-          [styles.buttonDisabled]: selected.length === 0,
-        })}
+        className={buttonClass(selected.length === 0)}
         onClick={handleConfirm}
         disabled={selected.length === 0}
       >
@@ -86,4 +68,27 @@ export default function Topics() {
       </Button>
     </div>
   );
+}
+
+function isTopic(value: string | null): value is TopicKey {
+  return value !== null && Object.keys(TOPIC_MAP).includes(value);
+}
+
+function getTopic(value: string | null): TopicKey | null {
+  if (isTopic(value)) {
+    return value;
+  }
+  return null;
+}
+
+function cardClass(selected: boolean) {
+  return clsx(styles.card, {
+    [styles.selected]: selected,
+  });
+}
+
+function buttonClass(disabled: boolean) {
+  return clsx(styles.button, {
+    [styles.buttonDisabled]: disabled,
+  });
 }
