6a07154bb21f54e05e80db8be3ca3da2d36fbb33
dmdeh <131264106+dmdeh@users.noreply.github.com>
2024-12-28 23:18:27 +0900

    refactor: useToggleSelection 구현 - toggle 유틸함수 삭제

--- src/app/selection/topics/contents/page.tsx
+++ src/app/selection/topics/contents/page.tsx
@@ -1,32 +1,24 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
+import { useRouter } from "next/navigation";
 import styles from "./contents.module.css";
-import { getTopic } from "@/app/utils/checkTopic";
 import { TOPIC_MAP, TopicKey } from "@/app/constants/topics";
-import { useState } from "react";
 import { SelectionCard } from "@/app/components/SelectionCard/SelectionCard";
 import clsx from "clsx";
 import Button from "@/app/components/common/Button/Button";
-import {
-  toggleSelectAll,
-  toggleSelectTopic,
-} from "@/app/utils/toggleSelection";
 import { theme } from "@/app/constants/theme";
+import useToggleSelection from "@/app/hooks/useToggleSelection";
 
 export default function Contents() {
-  const searchParams = useSearchParams();
   const router = useRouter();
-  const [selected, setSelected] = useState<string[]>([]);
-
-  const topic = getTopic(searchParams.get("topic")) ?? "Frontend";
-  const selectedTopics = TOPIC_MAP[topic];
-
-  const contents = searchParams.get("selected")?.split(",") || [];
-  const topicContents = getTopicContents(selectedTopics, contents);
-
-  const allSelected = selected.length === topicContents.length;
-  const notSelected = selected.length === 0;
+  const {
+    topicContents,
+    selected,
+    allSelected,
+    notSelected,
+    toggleSelectAll,
+    toggleSelectTopic,
+  } = useToggleSelection("contents");
 
   const handleConfirm = () => {
     if (!selected.length) return;
@@ -36,18 +28,14 @@ export default function Contents() {
   return (
     <div className={styles.page}>
       <div className={styles.header}>
-        <h1>
-          주제를 선택해주세요! {selected.length} / {topicContents.length}
-        </h1>
+        <h1>주제를 선택해주세요! {selected.length} / {topicContents.length}</h1>
         <p>당신을 위한 맞춤형 면접이 진행됩니다.</p>
       </div>
       <div className={styles.grid}>
         <SelectionCard
           title="전체 선택"
           isSelected={allSelected}
-          onClick={() =>
-            toggleSelectAll(allSelected, setSelected, topicContents)
-          }
+          onClick={() => toggleSelectAll(topicContents)}
           className={cardClass(allSelected)}
         />
         {topicContents.map((item) => {
@@ -58,7 +46,7 @@ export default function Contents() {
               key={item}
               title={item}
               isSelected={isSelected}
-              onClick={() => toggleSelectTopic(item, selected, setSelected)}
+              onClick={() => toggleSelectTopic(item)}
               className={cardClass(isSelected)}
             />
           );
@@ -90,7 +78,10 @@ function buttonClass(disabled: boolean) {
   });
 }
 
-function getTopicContents(topics: (typeof TOPIC_MAP)[TopicKey], selectedContents: string[]) {
+export function getTopicContents(
+  topics: (typeof TOPIC_MAP)[TopicKey],
+  selectedContents: string[]
+) {
   return Object.entries(topics)
     .filter(([key]) => selectedContents.includes(key))
     .reduce<string[]>((acc, [_, value]) => {
--- src/app/selection/topics/page.tsx
+++ src/app/selection/topics/page.tsx
@@ -1,34 +1,28 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useState } from "react";
+import { useRouter } from "next/navigation";
 import styles from "./topics.module.css";
-import { TOPIC_MAP } from "@/app/constants/topics";
 import Button from "@/app/components/common/Button/Button";
 import clsx from "clsx";
 import { SelectionCard } from "@/app/components/SelectionCard/SelectionCard";
-import { getTopic } from "@/app/utils/checkTopic";
-import {
-  toggleSelectAll,
-  toggleSelectTopic,
-} from "@/app/utils/toggleSelection";
 import { theme } from "@/app/constants/theme";
+import useToggleSelection from "@/app/hooks/useToggleSelection";
 
 export default function Topics() {
-  const searchParams = useSearchParams();
   const router = useRouter();
-  const [selected, setSelected] = useState<string[]>([]);
-
-  const topic = getTopic(searchParams.get("topic"));
-  const selectedTopics = topic ? TOPIC_MAP[topic] : {};
-  const allTopics = Object.keys(selectedTopics);
-  const isAllSelected = selected.length === allTopics.length;
+  const {
+    topic,
+    allTopics,
+    selected,
+    allSelected,
+    notSelected,
+    toggleSelectAll,
+    toggleSelectTopic,
+  } = useToggleSelection("topics");
 
   const handleConfirm = () => {
     if (!selected.length) return;
-    router.push(
-      `/selection/topics/contents?topic=${topic}&selected=${selected.join(",")}`
-    );
+    router.push(`/selection/topics/contents?topic=${topic}&selected=${selected.join(",")}`);
   };
 
   return (
@@ -39,27 +33,31 @@ export default function Topics() {
       <div className={styles.grid}>
         <SelectionCard
           title="전체 선택"
-          isSelected={isAllSelected}
-          onClick={() => toggleSelectAll(isAllSelected, setSelected, allTopics)}
-          className={cardClass(isAllSelected)}
+          isSelected={allSelected}
+          onClick={() => toggleSelectAll(allTopics)}
+          className={cardClass(allSelected)}
         />
-        {allTopics.map((topic) => (
-          <SelectionCard
-            key={topic}
-            title={topic}
-            isSelected={selected.includes(topic)}
-            onClick={() => toggleSelectTopic(topic, selected, setSelected)}
-            className={cardClass(selected.includes(topic))}
-          />
-        ))}
+        {allTopics.map((topic) => {
+          const isSelected = selected.includes(topic);
+
+          return (
+            <SelectionCard
+              key={topic}
+              title={topic}
+              isSelected={isSelected}
+              onClick={() => toggleSelectTopic(topic)}
+              className={cardClass(isSelected)}
+            />
+          );
+        })}
       </div>
       <Button
         backgroundColor={theme.colors.background}
         width={100}
         height={50}
-        className={buttonClass(selected.length === 0)}
+        className={buttonClass(notSelected)}
         onClick={handleConfirm}
-        disabled={selected.length === 0}
+        disabled={notSelected}
       >
         확인
       </Button>
