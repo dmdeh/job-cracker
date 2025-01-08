f7ea69e2b33d959406a315f69c1d9b3758b2c71e
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-01-08 11:20:31 +0900

    refactor: fetchQuestion 유효성 체크

--- src/app/hooks/useQuestion.ts
+++ src/app/hooks/useQuestion.ts
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { fetchQuestion } from "../services/question";
+import { fetchQuestion } from "../services/fetchQuestion";
 
 export function useQuestion(keywords: string[]) {
   const [isLoading, setIsLoading] = useState(true);
