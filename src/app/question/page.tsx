51cea7076527c41fc676f63a06543b3a32b95ace
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-01-08 14:03:02 +0900

    refactor: answer 상태 ref 적용 및 함수명 수정

--- src/app/question/page.tsx
+++ src/app/question/page.tsx
@@ -6,11 +6,12 @@ import styles from "./question.module.css";
 import useToggleSelection from "../hooks/useToggleSelection";
 import { useQuestion } from "../hooks/useQuestion";
 import shuffleArray from "../utils/shuffleArray";
-import { useMemo, useState } from "react";
+import { useMemo, useRef } from "react";
 
 export default function Question() {
   const searchParams = useSearchParams();
-  const [answer, setAnswer] = useState("");
+  const answer = useRef("");
+  const textareaRef = useRef<HTMLTextAreaElement>(null);
 
   const contents = searchParams.get("contents");
   const { topicContents } = useToggleSelection("contents");
@@ -30,19 +31,29 @@ export default function Question() {
 
   const getQuestionMessage = () => {
     if (!hasMoreQuestions) return "질문이 끝났습니다. 수고하셨습니다.";
-    return question || "질문을 불러오지 못했습니다.";
+    return question ?? "질문을 불러오지 못했습니다.";
   };
 
-  const handleSubmit = () => {
-    if (!answer.trim()) return;
-    getTailQuestion(answer);
-    setAnswer("");
+  const handleAnswerSubmit = () => {
+    const currentAnswer = answer.current.trim();
+    if (!currentAnswer) return;
+
+    getTailQuestion(currentAnswer);
+    answer.current = "";
+    if (textareaRef.current) {
+      textareaRef.current.value = "";
+    }
   };
 
   const handleKeyDown = (e: React.KeyboardEvent) => {
-    if (e.key === "Enter") {
-      handleSubmit();
+    if (e.key === "Enter" && !e.shiftKey) {
+      e.preventDefault();
+      handleAnswerSubmit();
     }
+  };
+
+  const handleAnswerUpdate = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+    answer.current = e.target.value;
   };
 
   return (
@@ -64,11 +75,11 @@ export default function Question() {
           id="answer"
           placeholder="답변을 입력해주세요..."
           className={styles.input}
-          value={answer}
-          onChange={(e) => setAnswer(e.target.value)}
+          ref={textareaRef}
+          onChange={handleAnswerUpdate}
           onKeyDown={handleKeyDown}
         />
-        <button className={styles.button} onClick={handleSubmit}>
+        <button className={styles.button} onClick={handleAnswerSubmit}>
           ⬆︎
         </button>
       </div>
