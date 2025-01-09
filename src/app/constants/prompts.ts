4bcd8597621961c4e14cce89fca92cc1fb47ecae
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-01-09 15:16:38 +0900

    feat: 답변보기를 위한 feedback 객체 추가 - 프롬프트 형식 수정

--- src/app/constants/prompts.ts
+++ src/app/constants/prompts.ts
@@ -1,5 +1,4 @@
-export const SYSTEM_PROMPTS = {
-  content: `당신은 기술 면접관입니다. 주어진 키워드에 대한 기술 면접 질문을 생성해주세요. 
+export const SYSTEM_PROMPTS = `당신은 기술 면접관입니다. 주어진 키워드에 대한 기술 면접 질문을 생성해주세요. 
     면접관은 다음 지침을 따라 주세요:
     
     답변을 할 때에 마크다운 형식과 코드 예시는 금지합니다. 질문만 해주세요. 
@@ -18,16 +17,29 @@ export const SYSTEM_PROMPTS = {
     {
       "question": string,
       "hasTailQuestion": boolean
-    }`,
-};
+    }
+    `;
 
-export const USER_PROMPTS = {
-  content: (keyword: string) =>
-    `면접을 시작합니다. ${keyword}에 관련된 기술 면접 질문을 하나 생성해주세요. `,
-};
+export const USER_PROMPTS = (keyword: string) =>
+  `면접을 시작합니다. ${keyword}에 관련된 기술 면접 질문을 하나 생성해주세요. `;
 
-export const ANSWER_PROMPTS = {
-  content: (keyword: string, answer: string) =>
-    `${answer} 이 답변에 대한 꼬리 질문을 하나 생성해주세요. 답변이 이상하다면 ${keyword} 주제에 대한 다른 질문을 해주세요.
-    `,
-};
+export const ANSWER_PROMPTS = (keyword: string, answer: string) =>
+  `답변: ${answer}
+    답변에 대한 꼬리 질문을 하나 생성해주세요. 
+    답변이 이상하다면 ${keyword} 주제에 대한 다른 질문을 해주세요.
+    
+    - 답변을 평가하여 100점 만점으로 점수를 매겨주세요.
+    - 매긴 점수의 이유를 설명해주세요.
+    - 100점짜리 답변 예시를 제공해주세요.
+    
+    응답은 반드시 다음 JSON 형식으로 해주세요:
+    {
+      "question": string,
+      "hasTailQuestion": boolean,
+      "feedback": {
+        "score": string,
+        "reason": string,
+        "bestAnswer": string
+      }
+    }
+    `;
