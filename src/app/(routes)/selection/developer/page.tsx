758c45046f97874cc6da69922fe7eb0175e3c9c3
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-01-14 18:12:52 +0900

    style: 스타일 수정

--- src/app/(routes)/selection/developer/page.tsx
+++ src/app/(routes)/selection/developer/page.tsx
@@ -7,7 +7,7 @@ export default function Developer() {
     <div className={layoutStyles.page}>
       <header className={layoutStyles.header}>
         <h1>당신은 어떤 개발자인가요?</h1>
-        <p>관심 있는 개발 분야를 선택해주세요</p>
+        <p>관심 있는 개발 분야를 선택해주세요.</p>
       </header>
       <main className={layoutStyles.list}>
         {Object.entries(developers).map(([key, developer]) => (
