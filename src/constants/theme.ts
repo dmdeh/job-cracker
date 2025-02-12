c6e563fc5c45c0ab5a8cd567d5bd7b579cb48fff
dmdeh <131264106+dmdeh@users.noreply.github.com>
2025-02-12 15:14:44 +0900

    style: 다른 주제 버튼 색상 변경

--- src/constants/theme.ts
+++ src/constants/theme.ts
@@ -1,11 +1,11 @@
 export const theme = {
   colors: {
-    background: "var(--color-background)",
-    backgroundLight: "var(--color-background-light)",
-    blue300: "var(--color-blue300)",
-    blue400: "var(--color-blue400)",
-    orange: "var(--color-orange)",
-    yellow: "var(--color-yellow)",
-    gray100: "var(--color-gray100)",
+    background: 'var(--color-background)',
+    backgroundLight: 'var(--color-background-light)',
+    blue300: 'var(--color-blue300)',
+    blue400: 'var(--color-blue400)',
+    pink: 'var(--color-pink)',
+    yellow: 'var(--color-yellow)',
+    gray100: 'var(--color-gray100)',
   },
 };
